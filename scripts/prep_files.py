d8c1a484956c07659e3bf651fdcc69043bcc355d
Laura Guillory <laura.a.guillory@gmail.com>
2019-10-16 12:15:17 +1000

    Using h5netcdf engine and Dask parallel features to read files slightly faster.

--- scripts/prep_files.py
+++ scripts/prep_files.py
@@ -129,7 +129,7 @@ def merge_years(dataset_name, file_path, logging_level=logging.INFO):
     LOGGER.info('Merging files for: ' + dataset_name)
     output_path = get_merged_dataset_path(file_path, dataset_name)
     inputs_path = file_path.format(dataset=dataset_name, year='*', filetype='nc')
-    with xarray.open_mfdataset(inputs_path, chunks={'time': 10}, combine='by_coords') as dataset:
+    with xarray.open_mfdataset(inputs_path, chunks={'time': 10}, combine='by_coords', parallel=True, engine='h5netcdf') as dataset:
         # Dimensions must be in this order to be accepted by the climate indices tool
         dataset = dataset.drop('crs', errors='ignore').transpose('lat', 'lon', 'time')
         encoding = {'time': {'units': 'days since 1889-01', '_FillValue': None}}
