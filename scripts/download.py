fee3829ad484baeb9e3d9bb78fcf004cb012dfb7
Laura Guillory <laura.a.guillory@gmail.com>
2019-10-21 14:27:29 +1000

    Added soil moisture to download options.

--- scripts/download.py
+++ scripts/download.py
@@ -11,11 +11,12 @@ LOGGER = logging.getLogger(__name__)
 
 
 DOWNLOAD_URLS = {'SILO': 'https://s3-ap-southeast-2.amazonaws.com/silo-open-data/annual/{dataset}/{year}.{dataset}.nc',
-                 'NDVI': 'http://www.bom.gov.au/web03/ncc/www/awap/ndvi/ndviave/month/grid/history/nat/{date_range}.Z'}
+                 'NDVI': 'http://www.bom.gov.au/web03/ncc/www/awap/ndvi/ndviave/month/grid/history/nat/{date_range}.Z',
+                 'soil_moisture': 'http://www.bom.gov.au/jsp/awra/thredds/fileServer/AWRACMS/values/month/sm_pct.nc'}
 DEFAULT_PATH = 'data/{dataset}/{date}.{dataset}.{filetype}'
 DATASET_CHOICES = ['daily_rain', 'et_morton_actual', 'et_morton_potential', 'et_morton_wet', 'et_short_crop',
                    'et_tall_crop', 'evap_morton_lake', 'evap_pan', 'evap_syn', 'max_temp', 'min_temp', 'monthly_rain',
-                   'mslp', 'radiation', 'rh_tmax', 'vp', 'vp_deficit', 'ndvi']
+                   'mslp', 'radiation', 'rh_tmax', 'vp', 'vp_deficit', 'ndvi', 'soil_moisture']
 
 
 def main():
@@ -34,17 +35,28 @@ def main():
         if dataset == 'ndvi':
             for year in range(1992, current_year + 1):
                 for month in range(1, 13):
-                    destination = path.format(dataset=dataset, date='{y}-{m:02d}'.format(y=year, m=month),
-                                              filetype='txt.Z')
-                    date_range = '{y}{m:02d}01{y}{m:02d}{d}'.format(y=year, m=month,
-                                                                    d=calendar.monthrange(year, month)[1])
+                    date = '{y}-{m:02d}'.format(y=year, m=month)
+                    day_of_year = calendar.monthrange(year, month)[1]
+                    date_range = '{y}{m:02d}01{y}{m:02d}{d}'.format(y=year, m=month, d=day_of_year)
+                    destination = path.format(dataset=dataset, date=date, filetype='txt.Z')
                     url = DOWNLOAD_URLS['NDVI'].format(date_range=date_range)
-                    try_to_download(url, destination, year)
+                    # Always redownload most recent year
+                    if file_already_downloaded(destination) and year != datetime.now().year:
+                        LOGGER.info('Already have {}'.format(url))
+                        continue
+                    try_to_download(url, destination)
+        elif dataset == 'soil_moisture':
+            destination = path.format(dataset=dataset, date='recent/recent', filetype='nc')
+            try_to_download(DOWNLOAD_URLS['soil_moisture'], destination)
         else:
             for year in range(1889, current_year + 1):
                 destination = path.format(dataset=dataset, date=year, filetype='nc')
                 url = DOWNLOAD_URLS['SILO'].format(dataset=dataset, year=year)
-                try_to_download(url, destination, year)
+                # Always redownload most recent year
+                if file_already_downloaded(destination) and year != datetime.now().year:
+                    LOGGER.info('Already have {}'.format(url))
+                    continue
+                try_to_download(url, destination)
     LOGGER.info('Done!')
 
 
@@ -71,10 +83,7 @@ def get_options():
     return parser.parse_args()
 
 
-def try_to_download(url, destination, year):
-    if os.path.isfile(destination) and year != datetime.now().year:  # Always redownload most recent year
-        LOGGER.info('Already have {}'.format(url))
-        return
+def try_to_download(url, destination):
     destination_dir = os.path.dirname(destination)
     os.makedirs(destination_dir, exist_ok=True)
     LOGGER.info('Downloading {} ...'.format(url))
@@ -97,6 +106,12 @@ def try_to_download(url, destination, year):
             remaining_download_tries -= 1
             continue
     LOGGER.warning('Download failed.')
+
+
+def file_already_downloaded(path):
+    if os.path.isfile(path):
+        return True
+    return False
 
 
 if __name__ == '__main__':
