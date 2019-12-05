b168acb4f6565fafe97075d6a68681189f24185b
Laura Guillory <laura.a.guillory@gmail.com>
2019-12-05 10:49:28 +1000

    get_lon_lat_names now works for Datasets from both netCDF4 and xarray modules.

--- utils/other.py
+++ utils/other.py
@@ -1,12 +1,19 @@
 import netCDF4
+import xarray
 
 
 def get_lon_lat_names(dataset: netCDF4.Dataset):
     """
     Try to guess what the longitude and latitude dimensions are called and raise an exception if it doesn't work.
     """
-    if all([element in dataset.dimensions for element in ['lon', 'lat']]):
+    if type(dataset) is netCDF4.Dataset:
+        dimensions = dataset.dimensions
+    elif type(dataset) is xarray.Dataset:
+        dimensions = dataset.dims
+    else:
+        raise ValueError('Must be given an xarray or netCDF4 Dataset.')
+    if all([element in dimensions for element in ['lon', 'lat']]):
         return 'lon', 'lat'
-    elif all([element in dataset.dimensions for element in ['longitude', 'latitude']]):
+    elif all([element in dimensions for element in ['longitude', 'latitude']]):
         return 'longitude', 'latitude'
     raise ValueError('Longitude and/or latitude dimensions not found.')
